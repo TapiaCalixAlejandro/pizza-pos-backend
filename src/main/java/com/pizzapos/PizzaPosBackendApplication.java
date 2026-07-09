0365baf669e891e89a03d58417353636f2d3af9e
TapiaCalixAlejandro <tapiacalix6@gmail.com>
2026-07-09 13:46:50 -0600

    chore: initial Spring Boot project

---
+++ src/main/java/com/pizzapos/PizzaPosBackendApplication.java
@@ -0,0 +1,13 @@
+package com.pizzapos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzaPosBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PizzaPosBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pizzapos/PizzaPosBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pizzapos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzaPosBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
